10a99067f8a1dc0fb9b2a259477d6cdf523631e4
FelixAL07 <152708404+FelixAL07@users.noreply.github.com>
2024-11-13 23:15:49 +0100

    Fix: Changed Links in HTML

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,19 @@
 const express = require("express");
+const path = require("path");//+
 const app = express();
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get("/", (req, res) => res.send("Express on Vercel"));//-
+// Serve static files from the 'public' directory//+
+app.use(express.static(path.join(__dirname, '..', 'public')));//+
 
+// Define routes//+
+app.get("/", (req, res) => {//+
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));//+
+});//+
+//+
+app.get("/about", (req, res) => {//+
+  res.sendFile(path.join(__dirname, '..', 'public', 'about.html'));//+
+});//+
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
 module.exports = app;
